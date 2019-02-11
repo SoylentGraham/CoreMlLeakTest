fef10f792644c12e9e29308ac80905da1ce773cc
Graham Reeves <graham@grahamreeves.com>
2019-02-11 15:29:57 +0000

    Demo project

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreMlLeakTest
+//
+//  Created by graham on 11/02/2019.
+//  Copyright © 2019 NewChromantics. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  CoreMlLeakTest
+//
+//  Created by graham on 11/02/2019.
+//  Copyright © 2019 NewChromantics. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
